3da25fb419adcc07d37f2a5726f38ed968cac1dd
ChenYC2 <105567780+ChenYC2@users.noreply.github.com>
2022-07-19 22:31:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+const count = 30;
+const apiKey = `TzMOZNsWjcsFXkWBi_TWfKmDkmeCR7kTPOvOg5k3MiU`
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imageLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        console.log('ready=', ready);
+    }
+}
+
+function setAttribute(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute (key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttribute(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        const img = document.createElement ('img');
+        setAttribute(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        }),
+        img.addEventListener('load', imageLoaded);
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+async function getPhotos () {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
+
+getPhotos();
